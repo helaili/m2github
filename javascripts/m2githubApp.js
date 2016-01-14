8c4060ff22fcf0a46e0d92e88f76ecd69dde4af4
Alain Hélaïli <helaili@github.com>
2016-01-14 19:58:46 +0100

    dealing with views

--- javascripts/m2githubApp.js
+++ javascripts/m2githubApp.js
@@ -12,7 +12,6 @@ angular.module('m2githubApp').config(['$interpolateProvider',
 
 angular.module('m2githubApp').config(['$stateProvider',
   function ($stateProvider) {
-    console.log('sweeeet');
     $stateProvider
       .state('status', {
         template: '<h1>My Contacts</h1>'
@@ -23,39 +22,13 @@ angular.module('m2githubApp').config(['$stateProvider',
 
 angular.module('m2githubApp').controller('DemoController', ['$scope', '$rootScope', '$stateParams',
   function ($scope, $rootScope, $stateParams) {
-    console.log('yessssss?xx');
-
-
-
-
-    $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
-      console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
-    });
-
-    $rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams){
-      console.log('$stateChangeError - fired when an error occurs during transition.');
-      console.log(arguments);
-    });
-
-    $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
-      console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
-    });
-
-    $rootScope.$on('$viewContentLoaded',function(event){
-      console.log('$viewContentLoaded - fired after dom rendered',event);
-    });
-
-    $rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
-      console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
-      console.log(unfoundState, fromState, fromParams);
-    });
 
 
     $scope.showStatus = function() {
       $scope.status = $stateParams.status;
       $scope.message = $stateParams.message;
       $scope.context = $stateParams.context;
-    }
+    };
 
   }
 ]);
