7f0b5a5567a48dc46b94483d8ef80a9135df47e6
Alain Hélaïli <helaili@github.com>
2016-01-14 19:43:55 +0100

    dealing with views

--- javascripts/m2githubApp.js
+++ javascripts/m2githubApp.js
@@ -32,7 +32,34 @@ angular.module('m2githubApp').config(['$stateProvider',
 
 angular.module('m2githubApp').controller('DemoController', ['$scope', '$stateParams',
   function ($scope, $stateParams) {
-    console.log('yessssss?');
+    console.log('yessssss?xx');
+
+
+        var $rootScope = angular.element(document.querySelectorAll("[ui-view]")[0]).injector().get('$rootScope');
+
+        console.log($rootScope);
+
+    $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
+      console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
+    });
+
+    $rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams){
+      console.log('$stateChangeError - fired when an error occurs during transition.');
+      console.log(arguments);
+    });
+
+    $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
+      console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
+    });
+
+    $rootScope.$on('$viewContentLoaded',function(event){
+      console.log('$viewContentLoaded - fired after dom rendered',event);
+    });
+
+    $rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
+      console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
+      console.log(unfoundState, fromState, fromParams);
+    });
 
 
     $scope.showStatus = function() {
