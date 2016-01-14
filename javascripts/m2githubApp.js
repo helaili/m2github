19dd14af9058374a220b0b511684d77f99c93377
Alain Hélaïli <helaili@github.com>
2016-01-14 11:30:01 +0100

    fixing ? Angular inc

--- javascripts/m2githubApp.js
+++ javascripts/m2githubApp.js
@@ -1,6 +1,9 @@
 var m2githubApp;
 m2githubApp = angular.module('m2githubApp', []);
 
+console.log('m2githubApp loaded');
+
+
 m2githubApp.config([
   '$interpolateProvider', function($interpolateProvider) {
     return $interpolateProvider.startSymbol('{(').endSymbol(')}');
