64a599a6b5de19dd4d4775f6a7da4d78f239e99e
Alain Hélaïli <helaili@github.com>
2016-01-14 12:22:05 +0100

    showing one stateparam

--- javascripts/m2githubApp.js
+++ javascripts/m2githubApp.js
@@ -1,16 +1,36 @@
 var m2githubApp = angular.module('m2githubApp', []);
 
-console.log('m2githubApp loaded');
-
-
-m2githubApp.config(function($interpolateProvider) {
+angular.module('m2githubApp').config(['$interpolateProvider',
+  function($interpolateProvider) {
     $interpolateProvider.startSymbol('//');
     $interpolateProvider.endSymbol('//');
-});
+  }
+]);
 
 
-angular.module('m2githubApp').controller('DemoController', ['$scope',
-  function ($scope) {
+angular.module('m2githubApp').config(['$stateProvider',
+  function ($stateProvider) {
+    $stateProvider
+      .state('status', {
+        abstract: true,
+        url: '/status',
+        template: '<ui-view/>'
+      })
+      .state('status.show', {
+        url: '?status&message&context',
+        templateUrl: 'show-status.html'
+      });
+  }
+]);
+
+
+angular.module('m2githubApp').controller('DemoController', ['$scope', , '$stateParams',
+  function ($scope, , $stateParams) {
     $scope.label = "This binding is brought you by // interpolation symbols.";
+
+    $scope.status = $stateParams.status;
+    $scope.message = $stateParams.message;
+    $scope.context = $stateParams.context;
+
   }
 ]);
