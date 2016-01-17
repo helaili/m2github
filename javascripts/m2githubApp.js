2ee8690b3f28add817b92de4cf66e269726e070c
Alain Hélaïli <helaili@github.com>
2016-01-17 19:54:50 +0100

    fixing path

--- javascripts/m2githubApp.js
+++ javascripts/m2githubApp.js
@@ -13,7 +13,7 @@ angular.module('m2githubApp').config(['$interpolateProvider', '$stateProvider',
         template: '<ui-view/>'
       })
       .state('status.slashm2github', {
-        url: '/test.html?message',
+        url: '?message',
         template: '<h1>My Contacts</h1>'
       })
       .state('status.slash', {
